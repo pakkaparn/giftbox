30b9080ced532c3841c66981bd88d636c899f873
Maetad Sukarasud <maetad91@gmail.com>
2018-12-14 14:32:07 +0700

    Add faker into test script

--- src/Api/v1/Services/WcmsUserService.php
+++ src/Api/v1/Services/WcmsUserService.php
@@ -6,12 +6,12 @@ use App\Models\WcmsUser;
 
 class WcmsUserService
 {
-    public function getByUsername($username)
+    public function getByUsername($username): WcmsUser
     {
-        return WcmsUser::whereUsername($username)->first();
+        return WcmsUser::whereUsername($username)->firstOrFail();
     }
 
-    public function verifyPassword(WcmsUser $user, $password)
+    public function verifyPassword(WcmsUser $user, $password): bool
     {
         return password_verify($password, $user->password);
     }
--- tests/Api/v1/Services/WcmsUserServiceTest.php
+++ tests/Api/v1/Services/WcmsUserServiceTest.php
@@ -4,19 +4,32 @@ namespace Test\Api\v1\Services;
 
 use App\Api\v1\Services\WcmsUserService;
 use App\Models\WcmsUser;
+use Faker\Factory as Faker;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use PHPUnit\Framework\TestCase;
 
 class WcmsUserServiceTest extends TestCase
 {
+    protected $faker;
+    protected $username;
+    protected $password;
+
     public function setUp()
     {
+        $this->faker = Faker::create();
+        $this->username = $this->faker->userName;
+        $this->password = $this->faker->password;
+
         WcmsUser::unguard();
+
         $this->user = WcmsUser::make([
-            'username' => 'admin',
-            'password' => 'password',
+            'username' => $this->username,
+            'password' => $this->password,
         ]);
 
         $this->user->save();
+
+        $this->service = new WcmsUserService;
     }
 
     public function tearDown()
@@ -24,17 +37,26 @@ class WcmsUserServiceTest extends TestCase
         WcmsUser::truncate();
     }
 
+    public function testGetByUsername()
+    {
+        $user = $this->service->getByUsername($this->username);
+        $this->assertSame($this->user->id, $user->id);
+    }
+
+    public function testGetWrongUsername()
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->service->getByUsername($this->faker->userName);
+    }
+
     public function testVerifyPassword()
     {
-        $service = new WcmsUserService;
-
-        $this->assertTrue($service->verifyPassword($this->user, 'password'));
+        $this->assertTrue($this->service->verifyPassword($this->user, $this->password));
     }
 
     public function testVerifyPasswordNotPass()
     {
-        $service = new WcmsUserService;
-
-        $this->assertFalse($service->verifyPassword($this->user, 'Password'));
+        $this->assertFalse($this->service->verifyPassword($this->user, $this->faker->password));
     }
 }
